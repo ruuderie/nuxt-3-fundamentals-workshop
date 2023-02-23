a45ee64dd2519af0153880a2e1db5b55920c8384
salym.eth <basset-zircon-0l@icloud.com>
2023-02-23 16:46:58 -0300

    adding photos

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,54 @@
+<script>
+import { defineNuxtComponent } from '#app';
+export default defineNuxtComponent({
+  name: 'App',
+  data: () => ({
+    message: 'Hello Nuxt.js',
+    todoList: [],
+    photosList:[]
+  }),
+  methods: {
+    fetchTodoList() {
+      fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response => response.json())
+        .then(json =>{
+          this.todoList = json;
+        });
+    },
+    fetchPhotos() {
+      fetch('https://jsonplaceholder.typicode.com/photos')
+        .then(response => response.json())
+        .then(json =>{
+          this.todoList = json;
+        });
+    },
+  }
+});
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <img src="@/salym.eth_todo.png" alt=""/>
+    <h1>Hello Frontend Masters!</h1>
+    <button @click="fetchTodoList">Fetch Todo List</button>
+   <!-- <pre> {{ todo }}</pre> -->
+    <ul>
+      <li v-for="item in todoList" :key="`todo-id-${item.id}`">
+        <input type="checkbox" :checked="item.completed" /> 
+        i{{ item.title }}
+      </li>
+    </ul>
+    <ul>
+      <li v-for="item in photosList" :key="`photo-id-${item.id}`">
+        <img src={{item.url}} alt=""/>
+      </li>
+    </ul>
   </div>
 </template>
+
+<style>
+img {
+  width: auto;
+  height: auto;
+}
+</style>
