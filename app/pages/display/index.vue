708fe9863e2e5fe82dd18e737456d56beeaf1128
salym.eth <basset-zircon-0l@icloud.com>
2023-02-25 18:19:25 -0300

    nuxt page routing changes

---
+++ app/pages/display/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+<template>
+    <div class="container">
+        <div class="section">
+        <h1>Display</h1>
+        <h2>Display App Placeholder</h2>
+        <NuxtPage />
+        </div>
+    </div>
+</template>
---
+++ app/pages/display/todos.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+<template>
+    <div class="container">
+        <TodoViewer></TodoViewer>
+    </div>
+</template>
